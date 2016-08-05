041b9d7176cd6ab75146efece836ccdcfb450c14
Jeff Burtoft <boyofgreen@gmail.com>
2016-08-05 11:50:50 -0500

    add css to handle the new layout on main page

--- frontpage.js
+++ frontpage.js
@@ -7,22 +7,61 @@
    {title:"b", id:2},
    {title:"c", id:3},
    {title:"d", id:4},
-   {title:"e", id:5}
+   {title:"e", id:5},
+   {title:"a", id:6},
+   {title:"b", id:7},
+   {title:"c", id:8},
+   {title:"d", id:9},
+   {title:"e", id:10}
  ]},
  {section:"Adventures", id:"B",movies:[
-    {title:"f", id:7},
-   {title:"g", id:8},
-   {title:"h", id:9},
-   {title:"i", id:10},
-   {title:"j", id:11}
+    {title:"f", id:11},
+   {title:"g", id:12},
+   {title:"h", id:13},
+   {title:"i", id:14},
+   {title:"j", id:15},
+      {title:"a", id:16},
+   {title:"b", id:17},
+   {title:"c", id:18},
+   {title:"d", id:19},
+   {title:"e", id:20}
    
  ]},
  {section:"People",id:"C", movies:[
-      {title:"k", id:12},
-   {title:"l", id:13},
-      {title:"k", id:14},
-   {title:"l", id:15},
-      {title:"k", id:16}
+   {title:"k", id:21},
+   {title:"l", id:22},
+   {title:"k", id:23},
+   {title:"l", id:24},
+   {title:"k", id:25},
+   {title:"a", id:26},
+   {title:"b", id:27},
+   {title:"c", id:28},
+   {title:"d", id:29},
+   {title:"e", id:30}
+ ]},
+ {section:"People",id:"D", movies:[
+   {title:"k", id:31},
+   {title:"l", id:32},
+   {title:"k", id:33},
+   {title:"l", id:34},
+   {title:"k", id:35},
+   {title:"a", id:36},
+   {title:"b", id:37},
+   {title:"c", id:38},
+   {title:"d", id:39},
+   {title:"e", id:40}
+ ]},
+ {section:"People",id:"E", movies:[
+   {title:"k", id:41},
+   {title:"l", id:42},
+   {title:"k", id:43},
+   {title:"l", id:44},
+   {title:"k", id:45},
+   {title:"a", id:46},
+   {title:"b", id:47},
+   {title:"c", id:48},
+   {title:"d", id:49},
+   {title:"e", id:50}
  ]}
    ]
  
@@ -44,9 +83,11 @@ var HeroBox = React.createClass({
 var MovieBox = React.createClass({
   render: function(){
     return(
-      <div className="movieBox" id={this.props.id} title={this.props.title}>
-       
+      <div className="movieBoxContainer">
+        <div className="movieBox" id={this.props.id} title={this.props.title}></div>
+        {this.props.title}
       </div>
+      
     )
   }
 });
