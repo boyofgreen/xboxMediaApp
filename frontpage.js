23ba3358dc04057587e8012d255930ef9966b475
Jeff Burtoft <boyofgreen@gmail.com>
2016-08-11 16:49:41 -0500

    update with image ref and back button working

--- frontpage.js
+++ frontpage.js
@@ -3,84 +3,99 @@
  
  var data = [
    {section:"recently released", id:"A", movies:[
-   {title:"a", id:1},
-   {title:"b", id:2},
-   {title:"c", id:3},
-   {title:"d", id:4},
-   {title:"e", id:5},
-   {title:"a", id:6},
-   {title:"b", id:7},
-   {title:"c", id:8},
-   {title:"d", id:9},
-   {title:"e", id:10}
+   {title:"Movie 1", id:1, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image09.jpg"},
+   {title:"Movie 1", id:2, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image14.jpg"},
+   {title:"Movie 1", id:3, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image19.jpg"},
+   {title:"Movie 1", id:4, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image26.jpg"},
+   {title:"Movie 1", id:5, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image21.jpg"},
+   {title:"Movie 1", id:6, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image09.jpg"},
+   {title:"Movie 1", id:7, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image09.jpg"},
+   {title:"Movie 1", id:8, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image09.jpg"},
+   {title:"Movie 1", id:9, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image09.jpg"},
+   {title:"Movie 1", id:10, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image09.jpg"},
  ]},
  {section:"Adventures", id:"B",movies:[
-    {title:"f", id:11},
-   {title:"g", id:12},
-   {title:"h", id:13},
-   {title:"i", id:14},
-   {title:"j", id:15},
-      {title:"a", id:16},
-   {title:"b", id:17},
-   {title:"c", id:18},
-   {title:"d", id:19},
-   {title:"e", id:20}
+   {title:"Movie 1", id:11, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image06.jpg"},
+   {title:"Movie 1", id:12, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image40.jpg"},
+   {title:"Movie 1", id:13, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image32.jpg"},
+   {title:"Movie 1", id:14, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image28.jpg"},
+   {title:"Movie 1", id:15, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image49.jpg"},
+   {title:"Movie 1", id:16, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeaturedImage_2x1_Image03.jpg"},
+   {title:"Movie 1", id:17, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image10.jpg"},
+   {title:"Movie 1", id:18, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image09.jpg"},
+   {title:"Movie 1", id:19, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image09.jpg"},
+   {title:"Movie 1", id:20, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image09.jpg"}
    
  ]},
  {section:"People",id:"C", movies:[
-   {title:"k", id:21},
-   {title:"l", id:22},
-   {title:"k", id:23},
-   {title:"l", id:24},
-   {title:"k", id:25},
-   {title:"a", id:26},
-   {title:"b", id:27},
-   {title:"c", id:28},
-   {title:"d", id:29},
-   {title:"e", id:30}
+   {title:"Movie 1", id:21, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image13.jpg"},
+   {title:"Movie l", id:22, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image16.jpg"},
+   {title:"Movie k", id:23, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image17.jpg"},
+   {title:"Movie l", id:24, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image22.jpg"},
+   {title:"Movie k", id:25, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image07.jpg"},
+   {title:"Movie a", id:26, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image09.jpg"},
+   {title:"Movie b", id:27, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image09.jpg"},
+   {title:"Movie c", id:28, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image09.jpg"},
+   {title:"Movie d", id:29, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image09.jpg"},
+   {title:"Movie e", id:30, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image09.jpg"}
  ]},
- {section:"People",id:"D", movies:[
-   {title:"k", id:31},
-   {title:"l", id:32},
-   {title:"k", id:33},
-   {title:"l", id:34},
-   {title:"k", id:35},
-   {title:"a", id:36},
-   {title:"b", id:37},
-   {title:"c", id:38},
-   {title:"d", id:39},
-   {title:"e", id:40}
+ {section:"The Great Outdoors",id:"D", movies:[
+   {title:"Movie k", id:31, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image48.jpg"},
+   {title:"Movie l", id:32, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image34.jpg"},
+   {title:"Movie k", id:33, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image22.jpg"},
+   {title:"Movie l", id:34, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image08.jpg"},
+   {title:"Movie k", id:35, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image18.jpg"},
+   {title:"Movie a", id:36, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image09.jpg"},
+   {title:"Movie b", id:37, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image09.jpg"},
+   {title:"Movie c", id:38, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image09.jpg"},
+   {title:"Movie d", id:39, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image09.jpg"},
+   {title:"Movie e", id:40, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image09.jpg"}
  ]},
- {section:"People",id:"E", movies:[
-   {title:"k", id:41},
-   {title:"l", id:42},
-   {title:"k", id:43},
-   {title:"l", id:44},
-   {title:"k", id:45},
-   {title:"a", id:46},
-   {title:"b", id:47},
-   {title:"c", id:48},
-   {title:"d", id:49},
-   {title:"e", id:50}
+ {section:"Kids",id:"E", movies:[
+   {title:"Movie k", id:41, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image37.jpg"},
+   {title:"Movie l", id:42, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image45.jpg"},
+   {title:"Movie k", id:43, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image12.jpg"},
+   {title:"Movie l", id:44, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image59.jpg"},
+   {title:"Movie k", id:45, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image24.jpg"},
+   {title:"Movie a", id:46, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image09.jpg"},
+   {title:"Movie b", id:47, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image09.jpg"},
+   {title:"Movie c", id:48, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image09.jpg"},
+   {title:"Movie d", id:49, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image09.jpg"},
+   {title:"Movie e", id:50, imgUrl:"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image09.jpg"}
  ]}
    ]
    
  //custom events
  var loadMoviePage = new CustomEvent('loadMoviePage');
  var playMovieFullScreen = new CustomEvent('playMovieFullScreen');
+ var currentPlayObj = {title:'Galaxy Explorer', imgUrl:'http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image04.jpg'};
+ 
+  var initStateObj = { state: "frontPage" };
+history.pushState(initStateObj, "page 2", "#frontPage");
+
+ 
+var loadMovieState = function(a){
+
+      var stateObj = { state: "playMovie" };
+history.pushState(stateObj, "page 2", "#playMovie");
+document.dispatchEvent(loadMoviePage)
+};
+
+
+ 
  
     
 var HeroBox = React.createClass({
   handleClick() {
-    document.dispatchEvent(loadMoviePage)
+     currentPlayObj = this.props;
+    loadMovieState()
   },
   render: function() {
     return (
       <div className="heroBox">
       <div className="controls">
             <h1>Galaxy Explorer</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fermentum ipsum sit amet porta faucibus. Quisque a diam id tellus placerat euismod. Cras ullamcorper a odio non rutrum. </p>
-                <button onClick={this.handleClick}>play</button>   
+                <button onClick={this.handleClick} ><span className="arrow-right">w</span></button>   
         </div>
       </div>
     );
@@ -89,12 +104,13 @@ var HeroBox = React.createClass({
 
 var MovieBox = React.createClass({
     handleClick: function() {
-    document.dispatchEvent(loadMoviePage)
+          currentPlayObj = this.props.data;
+    loadMovieState()
   },
   render: function(){
     return(
       <div className="movieBoxContainer">
-        <div className="movieBox" id={this.props.id} title={this.props.title} onClick={this.handleClick}></div>
+        <div className="movieBox" id={this.props.id} title={this.props.title} onClick={this.handleClick} style={{backgroundImage: 'url('+this.props.imgUrl+')'}}></div>
         {this.props.title}
       </div>
       
@@ -108,7 +124,7 @@ var MovieSection = React.createClass({
    var movieSectionNodes = this.props.data.map(function(data) {
       return (
         
-          <MovieBox id={data.id} key={data.id} title={data.title} />
+          <MovieBox id={data.id} key={data.id} title={data.title} data={data} imgUrl={data.imgUrl} />
         
       );
     });
@@ -148,9 +164,9 @@ var PlayMovie = React.createClass({
   render: function(){
     return(
       <div className="playMovie">
-        <div className="moviePlayer"></div>
+        <div className="moviePlayer" style={{backgroundImage: 'url('+this.props.data.imgUrl+')'}}></div>
         <div className="movieText">
-            <h2>Mystery Man</h2>
+            <h2>{this.props.data.title}</h2>
            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fermentum ipsum sit amet porta faucibus. Quisque a diam id tellus placerat euismod. Cras ullamcorper a odio non rutrum.</p>
         </div>
       </div>
@@ -160,9 +176,10 @@ var PlayMovie = React.createClass({
 
 var PlayMovieDetails = React.createClass({
   render:function(){
+   // console.log('movie obj', this.props)
     return(
       <div className="movieText">
-            <h2>Mystery Man</h2>
+            <h2>{this.props.data.title}</h2>
            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fermentum ipsum sit amet porta faucibus. Quisque a diam id tellus placerat euismod. Cras ullamcorper a odio non rutrum.</p>
       </div>
     )
@@ -175,28 +192,31 @@ class TopSection extends React.Component {
     this.state = {
       viewer: 'frontPage'
     };
-  //   this.handleClick = this.handleClick.bind(this);
    };
-  //   handleClick() {
-  //   this.setState({viewer: 'playMovie'});
-  // }
   componentDidMount(){
     var that = this;
+    window.addEventListener('popstate', function(e){
+      var stateObj = e.state;
+      window.scrollTo(0, 0);
+      that.setState({viewer: stateObj.state});
+      
+    });
     document.addEventListener('loadMoviePage', function(){
       that.setState({viewer: 'playMovie'});
+     // console.log(currentPlayObj)
     })
   }
   render() {
    if (this.state.viewer === 'frontPage'){
           return(
       <div className="topSection" >
-      <HeroBox />
+      <HeroBox title={"Galaxy Explorer"} id={0} imgUrl={"http://adx.metulev.com/video/Images/Clean/Small/FeatureImage_2x1_Image04.jpg"} />
       </div>
     )
     }else{
       return(
-      <div className="topSection">
-      <PlayMovie />
+      <div className="topSection" >
+      <PlayMovie data={currentPlayObj} />
       </div>
     )}
   }};
@@ -211,12 +231,20 @@ class BottomSection extends React.Component {
 
   componentDidMount(){
     var that = this;
+    window.addEventListener('popstate', function(e){
+      var stateObj = e.state;
+      window.scrollTo(0, 0);
+      that.setState({viewer: stateObj.state});
+      
+    });
     document.addEventListener('loadMoviePage', function(){
       window.scrollTo(0, 0);
       that.setState({viewer: 'playMovie'});
     })
   }
   render() {
+ 
+ 
    if (this.state.viewer === 'frontPage'){
           return(
       <div className="bottomSection" >
@@ -226,7 +254,7 @@ class BottomSection extends React.Component {
     }else{
       return(
       <div className="bottomSection">
-      <PlayMovieDetails />
+      <PlayMovieDetails data={currentPlayObj} />
       </div>
     )}
   }};
@@ -254,6 +282,6 @@ var PageWrapper = React.createClass({
 
 
 ReactDOM.render(
-  <PageWrapper data={data} />,
+  <PageWrapper data={data}  />,
   document.getElementById('page')
 );
